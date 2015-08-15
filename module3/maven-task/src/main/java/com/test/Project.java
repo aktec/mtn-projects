8531d1b32750c01796a998ba281038482f2abbff
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 05:52:40 +0300

    try to check sonar rule

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -6,11 +6,12 @@ import org.apache.commons.collections.Predicate;
 class Project {
 
   //void test
-  //do_nothing
+  // do_nothing
   public static void test() {
     System.out.println("test");
   }
   //void main
+  // study
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
