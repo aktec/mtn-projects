c61bdcd0a664d81b04198e95b1193b1317e06efb
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 04:01:57 +0300

    remove dependency. Added some comment at Project.class

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -5,10 +5,11 @@ import org.apache.commons.collections.Predicate;
 
 class Project {
 
+  //void test
   public static void test() {
     System.out.println("test");
   }
-  
+  //void main
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
